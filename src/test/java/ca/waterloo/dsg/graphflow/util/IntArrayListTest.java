4bee2348fb5eb2618720e0f45f617d80eda72a22
chathura-g21 <chathura.spoof@gmail.com>
2016-10-16 17:49:27 -0400

    adds prmitive arraylist

---
+++ src/test/java/ca/waterloo/dsg/graphflow/util/IntArrayListTest.java
@@ -0,0 +1,25 @@
+package ca.waterloo.dsg.graphflow.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests the IntArrayList class
+ */
+public class IntArrayListTest {
+
+  @Test
+  public void sort() throws Exception {
+
+    int[] unsortedList = {4, 7, 23, 76, 2, 56, 23, 78, 45};
+    IntArrayList testList = new IntArrayList();
+    testList.addAll(unsortedList);
+    testList.sort();
+    int[] sortedList = {2, 4, 7, 23, 23, 45, 56, 76, 78};
+
+    Assert.assertArrayEquals(sortedList, testList.toArray());
+  }
+
+}

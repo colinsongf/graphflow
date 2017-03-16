e862d5d27f2ddbaa44093765f117b5704c1218ac
Siddhartha Sahu <s3sahu@uwaterloo.ca>
2017-03-16 12:27:03 -0400

    Fix CLI to support bash pipes

--- src/main/java/ca/waterloo/dsg/graphflow/client/cli/GraphflowCli.java
+++ src/main/java/ca/waterloo/dsg/graphflow/client/cli/GraphflowCli.java
@@ -10,6 +10,8 @@ import org.jline.terminal.TerminalBuilder;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Client side CLI implementation, which communicates with the {@code GraphflowServer} using gRPC.
@@ -18,51 +20,59 @@ public class GraphflowCli extends GraphflowClient {
 
     private static final String PRIMARY_PROMPT = "graphflow> ";
     private static final String SECONDARY_PROMPT = "... ";
+    private static final char START_OF_COMMENT_LINE = '#';
 
     private LineReader lineReader;
 
     /**
-     * Construct a client connecting to a server at {@code host:port}.
+     * Constructs a client that connects to the Graphflow server at {@code host:port}.
+     *
+     * @param host The gRPC server hostname.
+     * @param port The gRPC server port.
      */
     public GraphflowCli(String host, int port) throws IOException {
         super(host, port);
+        Logger.getLogger("org.jline").setLevel(Level.OFF);
         lineReader = LineReaderBuilder.builder().terminal(TerminalBuilder.builder().build()).
             build();
     }
 
     /**
-     * Start the CLI interface.
+     * Starts the CLI interface.
      */
     public void startCLI() throws FileNotFoundException {
         String prompt = PRIMARY_PROMPT;
         String singleLineQuery;
-        String fullQuery = "";
+        StringBuilder stringBuilder = new StringBuilder();
         while (true) {
             try {
                 singleLineQuery = lineReader.readLine(prompt);
             } catch (UserInterruptException e) {
-                prompt = PRIMARY_PROMPT; // Reset prompt.
+                prompt = PRIMARY_PROMPT;
                 continue;
             } catch (EndOfFileException e) {
                 break;
             }
-            if (singleLineQuery.isEmpty()) {
+            if (null == singleLineQuery) {  // EOF.
+                break;
+            }
+            singleLineQuery = singleLineQuery.trim();
+            if (singleLineQuery.isEmpty() || START_OF_COMMENT_LINE == singleLineQuery.charAt(0)) {
                 continue;
             }
-            fullQuery += singleLineQuery;
+            stringBuilder.append(" ").append(singleLineQuery);
             if (!singleLineQuery.contains(";")) {
-                // The line does not contain semicolon. Change prompt and wait for more input
-                // before processing it.
                 prompt = SECONDARY_PROMPT;
                 continue;
             }
-            prompt = PRIMARY_PROMPT; // Reset prompt.
+            prompt = PRIMARY_PROMPT;
+            String fullQuery = stringBuilder.toString();
+            stringBuilder.setLength(0);
             if (fullQuery.contains("exit") || fullQuery.contains("quit")) {
                 break;
             }
             String result = queryServer(fullQuery);
             System.out.println("\nResult:\n" + result);
-            fullQuery = "";
         }
         System.out.println("May the flow be with you!");
     }
